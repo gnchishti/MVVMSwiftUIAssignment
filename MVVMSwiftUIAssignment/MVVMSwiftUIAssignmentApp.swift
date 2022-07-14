59c8d1750ec6ace3f29f4660d0f57ea395ab0c0c
gnchishti <gchishti@gmail.com>
2022-07-14 22:23:23 +0500

    Initial Commit

---
+++ MVVMSwiftUIAssignment/MVVMSwiftUIAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSwiftUIAssignmentApp.swift
+//  MVVMSwiftUIAssignment
+//
+//  Created by gnc on 14/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSwiftUIAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
